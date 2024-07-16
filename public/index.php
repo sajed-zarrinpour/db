54e8c384574a2656aa67a032ed84f9b59e5bfc1c
Sajed Zarinpour Nashroudkoli <sa.zarinpour@gmail.com>
2024-07-16 10:43:53 -0400

    added examples

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Models\User;
+
+// get all users
+var_dump(User::all());
+
+
+
+// create a new user
+$user = new User();
+$user->name = "amir";
+$user->save();
+
+var_dump($user);
+
+// get one user
+$user = User::find(38);
+var_dump($user);
+// update the user
+$user->name="majid";
+$user->save();
+
+var_dump(json_encode($user));
+
+// delete the user
+// $user->delete();
+// unset($user);
