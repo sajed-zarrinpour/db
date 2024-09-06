6b26fbc289117e7b0ca625a97974faafc83b9e70
Sajed Zarinpour Nashroudkoli <sa.zarinpour@gmail.com>
2024-09-06 16:07:10 -0400

    set the configuration for testing for github actions (Hopefully)

--- src/config/config.php
+++ src/config/config.php
@@ -10,19 +10,37 @@ use SajedZarinpour\DB\config\envLoader;
 if (!function_exists('config')) {
     function config(string $key): string
     {
-        $envl = new envLoader;
-        $envl(__DIR__ . '/../../../../..');
+        if(isset($_ENV['TEST']) && $_ENV['TEST'] == true)
+        {
+            $configs = [
+                'host' => '127.0.0.1',
+                'port' => '3306',
+                'database' => 'test_db',
+                'user' => 'root',
+                'password' => 'root',
+            ];
+    
+            if (!isset($configs[$key]))
+                throw new \Exception('requested config key not found');
+            return $configs[$key];
+        }
+        else
+        {
 
-        $configs = [
-            'host' => getenv('DB_HOST') ?? '127.0.0.1',
-            'port' => getenv('DB_PORT') ?? '3306',
-            'database' => getenv('DB_DATABASE') ?? 'laravel',
-            'user' => getenv('DB_USER') ?? 'dev',
-            'password' => getenv('DB_PASSWORD') ?? 'password',
-        ];
-
-        if (empty($configs[$key]))
-            throw new \Exception('requested config key not found');
-        return $configs[$key];
+            $envl = new envLoader;
+            $envl(__DIR__ . '/../../../../..');
+            
+            $configs = [
+                'host' => getenv('DB_HOST') ?? '127.0.0.1',
+                'port' => getenv('DB_PORT') ?? '3306',
+                'database' => getenv('DB_DATABASE') ?? 'laravel',
+                'user' => getenv('DB_USER') ?? 'dev',
+                'password' => getenv('DB_PASSWORD') ?? 'password',
+            ];
+    
+            if (!isset($configs[$key]))
+                throw new \Exception('requested config key not found');
+            return $configs[$key];
+        }
     }
 }
