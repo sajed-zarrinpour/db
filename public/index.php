73f2b96f5cb9260d426a4b8d0b03d723a22c6240
Sajed Zarinpour Nashroudkoli <sa.zarinpour@gmail.com>
2024-09-06 07:10:34 -0400

    nothing special

--- public/index.php
+++ public/index.php
@@ -4,44 +4,36 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Models\User;
 
-// $one = new stdClass();
-// $one->first = 'test';
-// $one->second = 'ronak';
-
-// $second = clone $one;
-
-// $one->first='varies';
-
-// var_dump($one);
-// var_dump($second);
-// die;
-
+// var_dump(User::fields());
 
 
 
 // var_dump(User::where('name','maj','like'));
 
-// get all users
+// // // get all users
 // var_dump(User::all());
 
 
 
 // // create a new user
-// $user = new User();
-// $user->name = "amir";
-// $user->save();
+$user = new User();
+$user->name = "amir";
+// $user->is_admin = true;
+$user->save();
 
-// var_dump($user);
-
-// // get one user
-$user = User::find(38);
 var_dump($user);
+
+// // // // get one user
+// $user = User::find($user->id);
+// var_dump($user);
+// var_dump(json_encode($user));
 // // update the user
 // $user->name = "majid";
+// $user->is_admin = false;
 // $user->save();
 
 // var_dump(json_encode($user));
 
-// // delete the user
-// // $user->delete();
-// // unset($user);
+// // // delete the user
+// $user->delete();
+// unset($user);
